613a2d1db88e038c3479146aa9e3c41109feea61
Sly Gryphon <sgryphon@computer.org>
2019-12-09 12:11:13 +1000

    Additional null logger comparison

--- test/LoggingPerformanceBenchmark/Program.cs
+++ test/LoggingPerformanceBenchmark/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Abstractions;
 
 namespace LoggingPerformanceBenchmark
 {
@@ -7,11 +8,17 @@ namespace LoggingPerformanceBenchmark
     {
         static void Main(string[] args)
         {
-            var clearProvidersFactory = LoggerFactory.Create(builder =>
+            ILoggerFactory clearProvidersFactory = LoggerFactory.Create(builder =>
             {
-                builder.ClearProviders();
+                builder
+                    .ClearProviders()
+                    .AddFilter("Events.None1", LogLevel.None)
+                    .AddFilter("Events.None2", LogLevel.None)
+                    .AddFilter("Events.Warning1", LogLevel.Warning)
+                    .AddFilter("Events.Warning2", LogLevel.Warning)
+                    .AddFilter("Events.Trace1", LogLevel.Trace);
             });
-            var eventSourceFactory = LoggerFactory.Create(builder =>
+            ILoggerFactory eventSourceFactory = LoggerFactory.Create(builder =>
             {
                 builder
                     .ClearProviders()
@@ -22,29 +29,57 @@ namespace LoggingPerformanceBenchmark
                     .AddFilter("Events.Trace1", LogLevel.Trace)
                     .AddEventSourceLogger();
             });
-            
-            var iterations = 1000000;
+            NullLoggerFactory nullLoggerFactory = new NullLoggerFactory();
+
+            int warmupCount = 1000;
+            int iterations = 1000000;
             //var iterations = 1000;
-            var nullRunner = new NullRunner();
-            var runners = new RunnerBase[] {
+            NullRunner nullRunner = new NullRunner();
+            RunnerBase[] runners = new RunnerBase[] {
                 new CountingRunner(),
-                new LoggerRunner("ClearProviders", clearProvidersFactory, "NoSource1", "NoSource2"),
-                new LoggerRunner("Eventing - None", eventSourceFactory, "Events.None1", "Events.None2"),
-                new LoggerRunner("Eventing - One Filt.", eventSourceFactory, "Events.Warning1", "Events.None2"),
-                new LoggerRunner("Eventing - Two Filt.", eventSourceFactory, "Events.Warning1", "Events.Warning2"),
                 new LoggerRunner("Eventing - One Full", eventSourceFactory, "Events.Trace1", "Events.Warning2"),
-                new LoggerMessageRunner("Eventing - None", eventSourceFactory, "Events.None1", "Events.None2"),
-                new LoggerMessageRunner("Eventing - One Filt.", eventSourceFactory, "Events.Warning1", "Events.None2"),
-                new LoggerMessageRunner("Eventing - Two Filt.", eventSourceFactory, "Events.Warning1", "Events.Warning2"),
+                new LoggerRunner("Eventing - Two Filt.", eventSourceFactory, "Events.Warning1", "Events.Warning2"),
+                new LoggerRunner("Eventing - One Filt.", eventSourceFactory, "Events.Warning1", "Events.None2"),
+                new LoggerRunner("Eventing - None", eventSourceFactory, "Events.None1", "Events.None2"),
+                
                 new LoggerMessageRunner("Eventing - One Full", eventSourceFactory, "Events.Trace1", "Events.Warning2"),
-                new GuardedLoggerRunner("Eventing - None", eventSourceFactory, "Events.None1", "Events.None2"),
-                new GuardedLoggerRunner("Eventing - One Filt.", eventSourceFactory, "Events.Warning1", "Events.None2"),
-                new GuardedLoggerRunner("Eventing - Two Filt.", eventSourceFactory, "Events.Warning1", "Events.Warning2"),
+                new LoggerMessageRunner("Eventing - Two Filt.", eventSourceFactory, "Events.Warning1", "Events.Warning2"),
+                new LoggerMessageRunner("Eventing - One Filt.", eventSourceFactory, "Events.Warning1", "Events.None2"),
+                new LoggerMessageRunner("Eventing - None", eventSourceFactory, "Events.None1", "Events.None2"),
+                
                 new GuardedLoggerRunner("Eventing - One Full", eventSourceFactory, "Events.Trace1", "Events.Warning2"),
+                new GuardedLoggerRunner("Eventing - Two Filt.", eventSourceFactory, "Events.Warning1", "Events.Warning2"),
+                new GuardedLoggerRunner("Eventing - One Filt.", eventSourceFactory, "Events.Warning1", "Events.None2"),
+                new GuardedLoggerRunner("Eventing - None", eventSourceFactory, "Events.None1", "Events.None2"),
+                
+                new LoggerMessageRunner("Eventing2 - One Full", eventSourceFactory, "Events.Trace1", "Events.Warning2"),
+                new LoggerMessageRunner("Eventing2 - Two Filt.", eventSourceFactory, "Events.Warning1", "Events.Warning2"),
+                new LoggerMessageRunner("Eventing2 - One Filt.", eventSourceFactory, "Events.Warning1", "Events.None2"),
+                new LoggerMessageRunner("Eventing2 - None", eventSourceFactory, "Events.None1", "Events.None2"),
+                
+                new GuardedLoggerRunner("Eventing2 - One Full", eventSourceFactory, "Events.Trace1", "Events.Warning2"),
+                new GuardedLoggerRunner("Eventing2 - Two Filt.", eventSourceFactory, "Events.Warning1", "Events.Warning2"),
+                new GuardedLoggerRunner("Eventing2 - One Filt.", eventSourceFactory, "Events.Warning1", "Events.None2"),
+                new GuardedLoggerRunner("Eventing2 - None", eventSourceFactory, "Events.None1", "Events.None2"),
+
+                new LoggerRunner("Eventing2 - One Full", eventSourceFactory, "Events.Trace1", "Events.Warning2"),
+                new LoggerRunner("Eventing2 - Two Filt.", eventSourceFactory, "Events.Warning1", "Events.Warning2"),
+                new LoggerRunner("Eventing2 - One Filt.", eventSourceFactory, "Events.Warning1", "Events.None2"),
+                new LoggerRunner("Eventing2 - None", eventSourceFactory, "Events.None1", "Events.None2"),
+
+                new LoggerRunner("ClearProviders - One Full", clearProvidersFactory, "Events.Trace1", "Events.Warning2"),
+                new LoggerRunner("ClearProviders - Two Filt.", clearProvidersFactory, "Events.Warning1", "Events.Warning2"),
+                new LoggerRunner("ClearProviders - One Filt.", clearProvidersFactory, "Events.Warning1", "Events.None2"),
+                new LoggerRunner("ClearProviders - None", clearProvidersFactory, "Events.None1", "Events.None2"),
+
+                new LoggerRunner("Null - One Full", nullLoggerFactory, "Events.Trace1", "Events.Warning2"),
+                new LoggerRunner("Null - Two Filt.", nullLoggerFactory, "Events.Warning1", "Events.Warning2"),
+                new LoggerRunner("Null - One Filt.", nullLoggerFactory, "Events.Warning1", "Events.None2"),
+                new LoggerRunner("Null - None", nullLoggerFactory, "Events.None1", "Events.None2"),
             };
 
             Console.WriteLine("Logging performance tester.");
-            Console.WriteLine("- Sends 10 warmup trace messages.");
+            Console.WriteLine("- Sends {0} warmup trace messages.", warmupCount);
             Console.WriteLine("- Then times the send of {0} trace messages.", iterations);
             Console.WriteLine("");
             Console.WriteLine("Typical configurations tested for each logging system:");
@@ -59,12 +94,12 @@ namespace LoggingPerformanceBenchmark
             Console.WriteLine("");
             Console.WriteLine("Warming up...");
             nullRunner.Output = false;
-            nullRunner.Iterations = 10;
+            nullRunner.Iterations = warmupCount;
             nullRunner.Run();
-            foreach (var runner in runners)
+            foreach (RunnerBase runner in runners)
             {
                 runner.Output = false;
-                runner.Iterations = 10;
+                runner.Iterations = warmupCount;
                 runner.Run();
             }
 
@@ -75,7 +110,7 @@ namespace LoggingPerformanceBenchmark
             nullRunner.Run();
 
             Console.WriteLine("");
-            foreach (var runner in runners)
+            foreach (RunnerBase runner in runners)
             {
                 runner.BaseTime = TimeSpan.Zero; // nullRunner.Elapsed;
                 runner.Output = true;
