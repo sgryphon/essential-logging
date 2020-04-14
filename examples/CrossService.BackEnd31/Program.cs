cce98b51d194cf12c18f2ef619f15f505ccea265
Sly Gryphon <sly@gamertheory.net>
2020-04-15 00:22:08 +1000

    Example with serilog + seq

--- examples/CrossService.BackEnd31/Program.cs
+++ examples/CrossService.BackEnd31/Program.cs
@@ -2,6 +2,7 @@ using System.Diagnostics;
 using Essential.LoggerProvider;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
+using Serilog;
 
 namespace CrossService.BackEnd31
 {
@@ -10,11 +11,19 @@ namespace CrossService.BackEnd31
         public static void Main(string[] args)
         {
             Activity.DefaultIdFormat = ActivityIdFormat.W3C;
+            
+            Serilog.Log.Logger = new LoggerConfiguration()
+                .Enrich.FromLogContext()
+                .WriteTo.Console()
+                .WriteTo.Seq("http://localhost:8123")
+                .CreateLogger();
+            
             CreateHostBuilder(args).Build().Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .UseSerilog()
                 .ConfigureLogging((context, loggingBuilder) =>
                 {
                     loggingBuilder.AddElasticsearch();
