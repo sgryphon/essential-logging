d5767113968d094fc36c57bc9cd7619be7159a6f
Sly Gryphon <sly@gamertheory.net>
2020-04-14 23:08:00 +1000

    Add more details to single tier hello web apps

--- examples/HelloWebApp31/UptimeService.cs
+++ examples/HelloWebApp31/UptimeService.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Diagnostics;
 using Microsoft.Extensions.Logging;
 
 namespace HelloWebApp31
@@ -18,6 +19,21 @@ namespace HelloWebApp31
 
         public TimeSpan GetUptime()
         {
+            _logger.LogInformation(2431,
+                "Outer31 Id {ActId}, TraceId {ActTraceId}, SpanId {ActSpanId}, ParentId {ActParentId}, RootId {ActRootId}",
+                Activity.Current?.Id, Activity.Current?.TraceId, Activity.Current?.SpanId, Activity.Current?.ParentId,
+                Activity.Current?.RootId);
+            var midActivity = new Activity("Mid");
+            midActivity.Start();
+            var innerActivity = new Activity("Inner");
+            innerActivity.Start();
+            _logger.LogInformation(2531,
+                "Inner31 Id {ActId}, TraceId {ActTraceId}, SpanId {ActSpanId}, ParentId {ActParentId}, RootId {ActRootId}",
+                Activity.Current?.Id, Activity.Current?.TraceId, Activity.Current?.SpanId, Activity.Current?.ParentId,
+                Activity.Current?.RootId);
+            midActivity.Stop();
+            innerActivity.Stop();
+
             var uptime = DateTimeOffset.Now - _started;
             Log.GetUptimeResult(_logger, uptime, null);
             return uptime;
